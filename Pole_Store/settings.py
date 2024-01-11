cc6af0f780412d1f0ba825e5222003b1e213dc85
Callum Morgan <callum@callums-air.home>
2024-01-11 13:10:10 +0000

    wire up POSTGRES database to project

--- Pole_Store/settings.py
+++ Pole_Store/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +33,9 @@ ALLOWED_HOSTS = [
     '.herokuapp.com',
 ]
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Application definition
 
@@ -72,15 +79,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Pole_Store.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
