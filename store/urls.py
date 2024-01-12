db5c0ef7d9a6ffeceac960a3eb45146a8c5ab710
Callum Morgan <callum@callums-air.home>
2024-01-12 13:34:23 +0000

    add item detail view

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.item_list, name='home' )
+    path('',views.item_list, name='home' ),
+    path('item/<int:item_id>/', views.item_detail, name='item_detail')
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 
 # Create your views here.
@@ -6,3 +6,8 @@ from .models import Item
 def item_list(request):
     Items = Item.objects.all()
     return render(request, "store/home.html", {"items":Items})
+
+
+def item_detail(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    return render(request, 'store/item_detail.html', {'item': item})
