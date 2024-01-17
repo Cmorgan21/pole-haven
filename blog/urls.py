cc5cc3f074646b05d9160dc5f545880488919fa1
Callum Morgan <callum@callums-air.home>
2024-01-17 17:32:13 +0000

    add blog details page functionality

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name="blog-page" )
+    path('', views.PostList.as_view(), name="blog-page" ),
+    path('<slug:slug>/', views.blog_details, name="blog_details")
 
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from.models import Post
 from django.views import generic
 
@@ -10,3 +10,13 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/blog_page.html"
     paginate_by = 6
+
+def blog_details(request, slug):
+    query = Post.objects.filter(status=1)
+    post= get_object_or_404(query, slug=slug)
+
+    return render(
+        request,
+        "blog/blog_detail.html",
+        {"post":post}
+    )
