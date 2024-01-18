1cfcd16578a5ff4e0e4608f8a44dac75a2b72361
Callum Morgan <callum@callums-air.home>
2024-01-18 01:46:42 +0000

    display created comments on blog details html

--- blog/models.py
+++ blog/models.py
@@ -25,6 +25,6 @@ class Comment(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
     approved = models.BooleanField(default=False)
 
--- blog/views.py
+++ blog/views.py
@@ -14,9 +14,13 @@ class PostList(generic.ListView):
 def blog_details(request, slug):
     query = Post.objects.filter(status=1)
     post= get_object_or_404(query, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
 
     return render(
         request,
         "blog/blog_detail.html",
-        {"post":post}
-    )
+        {"post":post,
+        "comments": comments,
+        "comment_count": comment_count,}
+        )
