b474ae3578740fe1188781eb2998341348747814
Callum Morgan <callum@callums-air.home>
2024-01-18 02:23:23 +0000

    add blog commenting styling and user authentication

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,6 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+        widgets = {
+            'body': forms.Textarea(attrs={'rows': 4, 'class': 'custom-textarea'}),
+        }
