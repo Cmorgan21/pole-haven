b3b2d0504440d9df027b2ffde7189fb5de32d890
Callum Morgan <callum@callums-air.home>
2024-01-26 19:42:08 +0000

    add styling to form.html

--- store/forms.py
+++ store/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Your Name', max_length=100)
-    email = forms.EmailField(label='Your Email', max_length=100)
-    message = forms.CharField(label='Your Message', widget=forms.Textarea)
+    subject_choices = [
+        ('general', 'General Inquiry'),
+        ('support', 'Customer Support'),
+        ('account', 'Account Support'),
+        ('advice', 'Poling Advice'),
+    ]
+
+    name = forms.CharField(max_length=100, label='Your Name')
+    email = forms.EmailField(label='Your Email')
+    subject = forms.ChoiceField(choices=subject_choices, label='Subject')
+    message = forms.CharField(widget=forms.Textarea, label='Your Message')
