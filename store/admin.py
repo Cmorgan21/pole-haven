a8ae3c73d114e9125abf63f21b9aa08c921be1ac
Callum Morgan <callum@callums-air.home>
2024-01-29 18:27:00 +0000

    add store admin pannel features

--- store/admin.py
+++ store/admin.py
@@ -3,7 +3,20 @@ from .models import Item, OrderItem, Order
 
 # Register your models here.
 
-admin.site.register(Item)
-admin.site.register(OrderItem)
-admin.site.register(Order)
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'price', 'discount_price')
+    search_fields = ('title', 'category')
+    list_filter = ('category',)
 
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('user', 'item', 'quantity', 'ordered', 'get_final_price')
+    list_filter = ('ordered',)
+    search_fields = ('item__title',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'ordered_date', 'get_total', 'ordered')
+    list_filter = ('ordered',)
+    search_fields = ('user__username',)
