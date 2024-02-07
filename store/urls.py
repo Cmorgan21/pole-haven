8dc73aed5bb6c9c3a92c621e2725b371d9c5a89b
Callum Morgan <callum@callums-air.home>
2024-02-07 21:48:32 +0000

    store urls pylint modification

--- store/urls.py
+++ store/urls.py
@@ -4,11 +4,12 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('',views.item_list, name='home' ),
+    path('', views.item_list, name='home'),
     path('item/<slug:slug>/', views.item_detail, name='item_detail'),
     path('add-to-cart/<slug>/', views.add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>/', views.remove_from_cart, name='remove-from-cart'),
     path('order-summary', views.order_summary, name='order-summary'),
     path('store/', views.store_view, name='online-store'),
     path('contact/', views.contact, name='contact'),
-    path('contact/success/', views.contact_success, name='contact_success')]
+    path('contact/success/', views.contact_success, name='contact_success')
+    ]
