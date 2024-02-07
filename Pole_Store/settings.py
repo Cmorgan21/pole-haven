671c4e9e83b92e496ad8c2ac2f660537eb2bae1d
Callum Morgan <callum@callums-air.home>
2024-02-07 22:21:54 +0000

    add secret key variable

--- Pole_Store/settings.py
+++ Pole_Store/settings.py
@@ -26,8 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3@l-5)kb8o0kxhv)_iu!rlpvl6_tc$xkobqb2y+yx)vp_(nfg2'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
