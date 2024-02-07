22631a99ee980c808edc8b1a7bff19917d2c761b
Callum Morgan <callum@callums-air.home>
2024-02-07 21:46:42 +0000

    store models pylint modification

--- store/models.py
+++ store/models.py
@@ -11,7 +11,9 @@ ITEM_CATEGORY = (
     ('ACC', 'Accessories')
 )
 
+
 class Item(models.Model):
+
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     price = models.FloatField()
@@ -22,32 +24,30 @@ class Item(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         return reverse("store:item_detail", kwargs={
-            'slug': self.slug
-        })
-    
+            'slug': self.slug})
+
     def get_add_to_cart_url(self):
         return reverse("store:add-to-cart", kwargs={
-            'slug': self.slug
-        })
-    
+            'slug': self.slug})
+
     def get_remove_from_cart_url(self):
         return reverse("store:remove-from-cart", kwargs={
-            'slug': self.slug
-        })
-    
+            'slug': self.slug})
+
     def get_category_display_name(self):
         # Get the display name for the category
         return dict(ITEM_CATEGORY)[self.category]
-    
+
     def savings(self):
         if self.discount_price:
             return self.price - self.discount_price
-    
+
     def __str__(self):
         return f"Comment {self.title} | {self.category} | {self.item_description}"
+
 
 class OrderItem(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -56,15 +56,16 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return f"{self.quantity} of {self.item.title}"  
+        return f"{self.quantity} of {self.item.title}"
 
     def get_final_price(self):
         if self.item.discount_price:
             return self.quantity * self.item.discount_price
         return self.quantity * self.item.price
-    
+
 
 class Order(models.Model):
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     items = models.ManyToManyField(OrderItem)
     start_date = models.DateTimeField(auto_now_add=True)
@@ -73,7 +74,6 @@ class Order(models.Model):
 
     def __str__(self):
         return f"{self.user}'s order {self.ordered_date}"
-    
+
     def get_total(self):
         return sum(item.get_final_price() for item in self.items.all())
-
