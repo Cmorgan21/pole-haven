330314b4301f73cc89c872dadd54927db624407a
Callum Morgan <callum@callums-air.home>
2024-02-07 18:08:42 +0000

    add comment edit functionality

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name="blog-page" ),
-    path('<slug:slug>/', views.blog_details, name="blog_details")
-
+    path('<slug:slug>/', views.blog_details, name="blog_details"),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.views import generic
 from .forms import CommentForm
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.urls import reverse
+
+
 
 
 
@@ -36,3 +41,27 @@ def blog_details(request, slug):
         "comment_count": comment_count,
         "comment_form": comment_form,}
         )
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('blog_details', args=[slug]))
+
